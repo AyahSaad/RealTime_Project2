b90f4c23af29733e04c5ae7e73eb1343e19e8c89
Your Name <you@example.com>
2024-01-08 02:48:13 +0300

    first commit

---
+++ customer.h
@@ -0,0 +1,11 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+#include "fileReaders.h"
+
+void simulateCustomerShopping(int customerID);
+void simulateCustomerArrival(int *shelfQuantities);
+int getRandomNumber(int lower, int upper);
+
+#endif
+
+
---
+++ customers.c
@@ -0,0 +1,63 @@
+#include "fileReaders.h"
+#include "customer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+
+int getRandomNumber(int lower, int upper) {
+    return rand() % (upper - lower + 1) + lower;
+}
+
+void simulateCustomerShopping(int customerID) {
+    printf("Customer %d started shopping.\n", customerID);
+
+
+    int shoppingTime = rand() % (CUSTOMER_SHOPPING_TIME_UPPER - CUSTOMER_SHOPPING_TIME_LOWER + 1) +
+                       CUSTOMER_SHOPPING_TIME_LOWER;
+
+    sleep(shoppingTime);
+
+    // Simulate choosing items and quantities
+    for (int i = 0; i < PRODUCT_COUNT; i++) {
+        pthread_mutex_lock(&products[i].productMutex);  
+        int quantity = getRandomNumber(1, products[i].initialAmountOnShelves);
+        printf("Customer %d picked %d units of %s.\n", customerID, quantity, products[i].name);
+        products[i].initialAmountOnShelves -= quantity;
+        pthread_mutex_unlock(&products[i].productMutex);  
+    }
+
+    printf("Customer %d finished shopping.\n", customerID);
+}
+
+void simulateCustomerArrival(int *shelfQuantities) {
+
+        int arrivalPid = getpid();
+        printf("I am arrival maker %d\n", arrivalPid);
+
+        srand(time(NULL) ^ (getpid() << 16));
+
+        int randArrival = getRandomNumber(CUSTOMER_ARRIVAL_RATE_LOWER, CUSTOMER_ARRIVAL_RATE_UPPER);
+
+        printf("Arrival time: %d\n", randArrival);
+
+  while (1)
+    {
+
+        if (getpid() == arrivalPid)
+        {
+
+            sleep(randArrival);
+            int forkAcustomer = fork();
+
+            if (forkAcustomer == 0) 
+            {
+                printf("I am customer %d \n", getpid());
+                simulateCustomerShopping(getpid());
+           
+            }
+        }
+    }
+
+}
