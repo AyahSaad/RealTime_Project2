f87995a83b0e56365f2945468120934afc940878
halaziq <halaziq@gmail.com>
2024-01-09 13:06:01 +0200

    Test

---
+++ openGLfunctions.h
@@ -0,0 +1,18 @@
+#ifndef openGL_H
+#define openGL_H
+
+extern int windowWidth;
+extern int windowHeight;
+extern int triangleWidth;
+extern int triangleHeight;
+extern int yOffset;
+extern int circleRadius;
+
+
+void drawItems(void);
+void drawWhiteRectangle(void);
+void drawCashier(int cashier, int index, int queueSize, int itemsCount,int behavior, int tpi);
+void display(void);
+void reshape(int w, int h);
+
+#endif
